b40c53f6c3cbf6123428deb3f00fe42167e349af
LailaCharaoui <mats2@live.fr>
2023-06-27 00:27:55 +0200

    feat(front): init vue and login

---
+++ QuizzBattle/front/src/components/GameHeader/GameHeader.vue
@@ -0,0 +1,44 @@
+<template>
+<RoundedSquare>
+<ProfileBadge color="red"/>
+<h2>{{gameName}}</h2>
+<ProfileBadge color="green"/>
+</RoundedSquare>
+</template>
+
+<script>
+import ProfileBadge from './../ProfileBadge.vue';
+import RoundedSquare from './../RoundedSquare.vue';
+export default {
+  props: {
+    color: {
+        type: String,
+        required: false,
+        default: "black"
+    },
+    gameName : {
+        type: String,
+        required: false,
+        default: "QuizzBattle"
+    },
+  },
+  components: {
+    ProfileBadge,
+    RoundedSquare,
+  }
+
+
+}
+</script>
+
+<style>
+
+    .player-band{
+        background-color: violet;
+        border-radius: 0.5rem;
+        padding: 1rem;
+        display: flex;
+        justify-content: space-between;
+       
+    }
+</style>
---
+++ QuizzBattle/front/src/components/ProfileBadge.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="flex items-center justify-center w-12 h-12 rounded-full overflow-hidden" :style="{
+    backgroundColor: color
+   }">
+  <img src="https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg" alt="Profil" class="w-full h-full object-cover" />
+</div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+        type: String,
+        required: false,
+        default: "red"
+    }
+  },
+
+
+}
+</script>
+
+<style>
+
+.player-band {
+    color: "black";
+    border-radius: 0.5rem;
+    padding: 1rem;
+}
+
+</style>

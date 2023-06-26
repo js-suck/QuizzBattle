1b77c2389b25dba07d10bde6cf1171418bab6e3f
LailaCharaoui <mats2@live.fr>
2023-06-27 00:27:55 +0200

    feat(front): init vue and login

--- QuizzBattle/front/src/router/index.ts
+++ QuizzBattle/front/src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'quizz',
+      component: () => import('../views/StartQuizz.vue')
     },
     {
       path: '/about',

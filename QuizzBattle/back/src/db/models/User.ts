8076639ced3abe5069ccaa1cf3bbd1d71e2b7e04
senex127 <antoine127chabernaud@gmail.com>
2023-07-18 15:44:51 +0200

    user verif mail + role

--- QuizzBattle/back/src/db/models/User.ts
+++ QuizzBattle/back/src/db/models/User.ts
@@ -1,9 +1,42 @@
 module.exports = (connection) => {
   const { DataTypes, Model } = require("sequelize");
   const bcrypt = require("bcrypt");
+  const Mailjet = require("node-mailjet");
+
+  const mailjetClient = new Mailjet({
+    apiKey: "dfaaa1f406a6a163ed3cfd1c77387ae4",
+    apiSecret: "7797beb3977eca8bba9c9fc327e45ab3",
+  });
+
   class User extends Model {
     async checkPassword(password) {
       return await bcrypt.compare(password, this.password);
+    }
+
+    async sendVerificationEmail() {
+      // Envoyer l'e-mail de vérification à l'utilisateur
+      const request = mailjetClient.post("send", { version: "v3.1" }).request({
+        Messages: [
+          {
+            From: {
+              Email: "votre-email@example.com",
+              Name: "Votre Nom",
+            },
+            To: [
+              {
+                Email: this.email,
+                Name: `${this.firstname} ${this.lastname}`,
+              },
+            ],
+            Subject: "Vérification de compte",
+            HTMLPart: `<p>Bonjour ${
+              this.firstname
+            },</p><p>Veuillez cliquer sur le lien suivant pour vérifier votre compte : <a href="${generateVerificationLink(
+              this.id
+            )}">Vérifier mon compte</a></p>`,
+          },
+        ],
+      });
     }
   }
 
@@ -30,7 +63,18 @@ module.exports = (connection) => {
       profilePicturePath: {
         type: DataTypes.STRING,
         allowNull: true,
-      }
+      },
+      role: {
+        type: DataTypes.STRING,
+        defaultValue: "user",
+        validate: {
+          isIn: [["user", "admin"]],
+        },
+      },
+      isVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
     },
     { sequelize: connection, tableName: "users" }
   );
@@ -43,7 +87,16 @@ module.exports = (connection) => {
     }
   }
 
+  function generateVerificationLink(userId) {
+    // Générer le lien de vérification avec l'ID de l'utilisateur
+    return `http://localhost:5173/verify/${userId}`;
+  }
+
   User.addHook("beforeCreate", uptadePassword);
+
+  User.addHook("afterCreate", (user) => {
+    user.sendVerificationEmail();
+  });
 
   return User;
 };

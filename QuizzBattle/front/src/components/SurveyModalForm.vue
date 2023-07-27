65885e74f7d6a74276d5a9c57a7d56434c08c408
ramis <lucas.ramis7@gmail.com>
2023-07-27 10:20:57 +0200

    :sparkless: Back office + gestion des users

--- QuizzBattle/front/src/components/SurveyModalForm.vue
+++ QuizzBattle/front/src/components/SurveyModalForm.vue
@@ -15,34 +15,49 @@
                 </v-btn>
             </template>
             <v-card>
-                <v-card-title>
-                    <span class="text-h5">New Category</span>
-                </v-card-title>
-                <v-card-text>
-                    <v-container>
-                        <v-row>
-                            <v-col
-                                cols="12"
-                                sm="6"
-                                md="6"
-                            >
-                                <v-text-field
-                                    label="Name"
-                                    required
-                                ></v-text-field>
-                            </v-col>
-                            <v-col
-                                cols="12"
-                                sm="6"
-                                md="6"
-                            >
-                                <v-text-field
-                                    label="Description"
-                                    persistent-hint
-                                    required
-                                ></v-text-field>
-                            </v-col>
-                        </v-row>
+                <form class="my-4 w-full" @submit.prevent="submitForm" enctype="multipart/form-data">
+                    <v-card-title>
+                        <span class="text-h5">New Category</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container>
+                            <v-row>
+                                <v-col
+                                    cols="12"
+                                    sm="6"
+                                    md="6"
+                                >
+                                    <v-text-field
+                                        v-model.lazy="categories.name"
+                                        label="Name"
+                                        required
+                                    ></v-text-field>
+                                </v-col>
+                                <v-col
+                                    cols="12"
+                                    sm="6"
+                                    md="6"
+                                >
+                                    <v-text-field
+                                        label="Description"
+                                        v-model.lazy="categories.description"
+                                        required
+                                    ></v-text-field>
+                                </v-col>
+                            <v-row>
+                                <v-col
+                                    cols="12"
+                                    sm="6"
+                                    md="6">
+                                    <v-file-input label="File input"
+                                          accept="image/*"
+                                          id="profilePictures"
+                                          name="profilePictures"
+                                          ref="fileInputRef"
+                                    ></v-file-input>
+                                </v-col>
+                            </v-row>
+                            </v-row>
                             <section v-for="(question, index) in newSection.questions" :key="index" id="addSectionQuestion">
                                 <span class="text-h6">Question {{ index + 1 }}</span>
                                 <v-row>
@@ -63,47 +78,53 @@
                                     </v-col>
                                 </v-row>
                             </section>
-                    </v-container>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                        color="blue-darken-1"
-                        variant="text"
-                        @click="addSectionQuestion"
-                    >
-                        Add Question
-                    </v-btn>
-                    <v-btn
-                        color="blue-darken-1"
-                        variant="text"
-                        @click="dialog = false"
-                    >
-                        Close
-                    </v-btn>
-                    <v-btn
-                        color="blue-darken-1"
-                        variant="text"
-                        @click="saveCategory"
-                    >
-                        Save
-                    </v-btn>
-                </v-card-actions>
+                        </v-container>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                            color="blue-darken-1"
+                            variant="text"
+                            @click="addSectionQuestion"
+                        >
+                            Add Question
+                        </v-btn>
+                        <v-btn
+                            color="blue-darken-1"
+                            variant="text"
+                            @click="dialog = false"
+                        >
+                            Close
+                        </v-btn>
+                        <v-btn
+                            color="blue-darken-1"
+                            variant="text"
+                            type="submit"
+                        >
+                            Save
+                        </v-btn>
+                    </v-card-actions>
+                </form>
+
             </v-card>
         </v-dialog>
     </v-row>
 </template>
 <script>
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import axios from "axios";
 import {API_URL} from "@/constants";
 
+const fileInputRef = ref(null);
 
 export default {
     data() {
         return {
             dialog: false,
-            categories: [],
+            categories: {
+                name: '',
+                description: '',
+            },
             newSection: {
                 name: '',
                 description: '',
@@ -131,6 +152,25 @@ export default {
             };
             this.dialog = false;
         },
+        submitForm() {
+            // Log all the questions to the console
+            console.log(this.categories, fileInputRef.value.files[0]);
+            /*axios.post(`${API_URL}/api/category/add`)
+                .then((response) => {
+                    responseUser.value = response.data;
+                    window.location.reload();
+
+                })
+                .catch((error) => {
+                    console.error('Error while fetching user data:', error);
+                });
+            this.newSection.questions.forEach((question, index) => {
+                console.log(`Question ${index + 1}: ${question.questionText}`);
+                console.log(`  Good Answer: ${question.goodAnswer}`);
+                console.log(`  Bad Answers: ${question.badAnswers.join(', ')}`);
+            });*/
+        }
+
     },
 }
 
